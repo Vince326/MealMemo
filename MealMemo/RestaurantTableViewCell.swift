ad63c4236d285deb542a040e8030446465eb6b1c
Vincent Hunter <jhearst29@gmail.com>
2025-02-22 21:58:26 -0500

    Initial Commit Comment

---
+++ MealMemo/RestaurantTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantTableViewCell.swift
+//  MealMemo
+//
+//  Created by Vincent Hunter on 2/22/25.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = 20.0
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.tintColor = .systemYellow
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MealMemo/RestaurantTableViewController.swift
@@ -0,0 +1,118 @@
+//
+//  RestaurantTableViewController.swift
+//  MealMemo
+//
+//  Created by Vincent Hunter on 2/22/25.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery",
+                           "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf",
+                           "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh",
+                            "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore","confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney","New York",
+                               "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian/ Causual Drink", "French", "Bakery", "Bakery", "Chocolate",
+                           "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var restaurantIsFavorites = Array(repeating: false, count: 21)
+    
+    
+    enum Section {
+        case all
+    }
+    
+    lazy var dataSource = configureDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = dataSource
+        tableView.separatorStyle = .none
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(restaurantNames, toSection: .all)
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+    // MARK: - Table view data source
+    
+    
+
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+        
+        let cellIdentifier = "datacell"
+        
+        let dataSource = UITableViewDiffableDataSource<Section, String>(tableView: tableView, cellProvider: {
+            tableView, indexPath, restaurantName in
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+            
+            //Configures the cell
+            cell.nameLabel.text = restaurantName
+            cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
+            cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+            cell.typeLabel.text = self.restaurantTypes[indexPath.row]
+            
+            cell.accessoryType = self.restaurantIsFavorites[indexPath.row] ? .checkmark : .none
+
+            return cell
+        }
+    )
+       return dataSource
+        }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //Creates an option menu as a sheet
+        
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        //Add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        //Add Reserve Table Action
+        let reserveActionHandler = { (action: UIAlertAction) -> Void in
+            
+            let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry, this feature isn't available yet", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        
+        let reserveAction = UIAlertAction(title: "Reserve Table", style: .default, handler: reserveActionHandler)
+        optionMenu.addAction(reserveAction)
+        
+        
+        //Mark as favorite action
+        let favoriteAction = UIAlertAction(title: "Mark as Favorite", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+            
+            self.restaurantIsFavorites[indexPath.row] = true
+        })
+        optionMenu.addAction(favoriteAction)
+        
+        //Display Action Menu
+        present(optionMenu, animated: true, completion: nil)
+        
+        //Deselect the row
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+    
+}
+    
